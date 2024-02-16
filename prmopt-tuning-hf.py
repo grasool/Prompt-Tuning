9e0ff58ef6e57caaf5f1658f3093a29cc019b430
grasool <mr.ghulamrasool@gmail.com>
2024-02-16 08:43:11 -0500

    update

--- prmopt-tuning-hf.py
+++ prmopt-tuning-hf.py
@@ -8,3 +8,14 @@ import os
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_name_or_path = "bigscience/bloomz-560m"
+tokenizer_name_or_path = "bigscience/bloomz-560m"
+
+peft_config = PromptTuningConfig(
+    task_type=TaskType.CAUSAL_LM,
+    prompt_tuning_init=PromptTuningInit.TEXT,
+    num_virtual_tokens=8,
+    prompt_tuning_init_text= "Classify if the tweet is a complaint or not:"
+)
